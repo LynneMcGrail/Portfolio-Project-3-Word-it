6ffc6382a48bbeafa14d7f3625b5cde2e019edce
Lynne McGrail <lynne.mcgrail@gmail.com>
2022-12-14 12:34:57 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Main run game file for 'Word-It'
+"""
+
+import sys
+import random
+
+# install & import pyfiglet module from https://www.geeksforgeeks.org/python-ascii-art-using-pyfiglet-module/
+import pyfiglet
+
+result = pyfiglet.figlet_format("Word-It", font = "digital")
+print(result)
