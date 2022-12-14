ec31fb2e35d898646daab1167c7f95522035e98b
Lynne McGrail <lynne.mcgrail@gmail.com>
2022-12-14 17:46:01 +0000

    Added word checker class and validation of user guesses

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ Main function to run game the file for 'Word-It'
 """
 
 import os
-import sys
 import pygame
 import pyfiglet
 import random
@@ -12,13 +11,13 @@ from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
 
-# install & import pyfiglet module from https://www.geeksforgeeks.org/python-ascii-art-using-pyfiglet-module/
+# install & import pyfiglet module from https://www.geeksforgeeks.org/python-ascii-art-using-pyfiglet-module/  # noqa
 
-result = pyfiglet.figlet_format("Word-It", font = "digital")
+result = pyfiglet.figlet_format("Word-It", font="digital")
 print(result)
 
 
-def get_word_from_file():
+def get_answer_from_file():
     """
     Open words.txt file and get random word
     for the user to guess when playing Word-It
@@ -63,10 +62,10 @@ class Game:
             "Can you guess the word in just 6 tries?\n".center(80))
         while True:
             self.username = input(
-                "Please enter your name to begin the game:\n").strip().capitalize()
+                "Please enter your name to begin the game:\n").strip().capitalize()  # noqa
 
             if len(self.username.strip()) == 0:
-                print(f"{Fore.RED}Username invalid, must contain letters or numbers.\n")
+                print(f"{Fore.RED}Username invalid, must contain letters or numbers.\n")  # noqa
             else:
                 break
         print(f"{Fore.MAGENTA}/nHello {self.username}, lets play Word-It!\n")
@@ -113,7 +112,6 @@ class Game:
         else:
             print(Fore.RED + "Not a valid option\n")
             self.user_menu()
-    
 
     def play_again(self):
         """
@@ -122,7 +120,7 @@ class Game:
         inputs invalid option.
         """
         print('Choose from the following options:\n')
-        user_choice = input (
+        user_choice = input(
             Fore.MAGENTA + "P - PLAY AGAIN\nQ - QUIT\n"
             ).strip().lower()
 
@@ -139,12 +137,65 @@ class Game:
             print(Fore.RED+"Not a valid option\n")
             self.play_again()
 
+    def ask_for_guess(self):
+        """
+        Use a while loop to collect a valid guess
+        from the user if they have guesses remaining.
+        If the user has no guesses remaining, the game
+        will finish and a goodbye message will display.
+        """
+        while self.no_of_guesses <= 6:
+            if self.no_of_guesses == 0:
+                print(' \n')
+                print("GAME OVER", justify="center", width=80)
+                fore = Fore.YELLOW + Style.BRIGHT
+                upper_answer = self.word_checker.answer.upper()
+                print(f"The answer was....{fore}{upper_answer}\n")
+                self.play_again()
+            else:
+                fore = Fore.YELLOW + Style.BRIGHT
+                chances = self.no_of_chances
+                reset = Fore.RESET + Style.RESET_ALL
+                print(f"\nYou have {fore}{chances}{reset} chances left.\n")
+            while True:
+                user_input = input(
+                    'Enter your 5 letter guess here:\n').lower().strip()
+                if self.word_checker.validate_user_guess(user_input):
+                    self.no_of_guesses -= 1
+                    self.display_guesses(user_input)
+                    if user_input == self.word_checker.answer:
+                        score = 6 - self.no_of_chances
+                        print(' \n')
+                        print(pyfiglet.figlet_format(
+                            "YOU WIN", justify="center", width=80))
+                        print('\nWell done you got the correct answer!\n')
+                        self.play_again()
+                    break
+
+    def display_guesses(self, user_input):
+        """
+        Display the user's guess and previous guesses one after
+        the other with color-codes through colorama.
+        """
+        current_guess = self.word_checker.check_matching_letters(user_input)
+        self.guesses_list.append(current_guess)
+
+        print(f"""\n{Fore.CYAN}\t\t\t      =====================\n""")
+        for i in self.guesses_list:
+            print("\t\t\t\t      "+i)
+        print(f"""\n{Fore.CYAN}\t\t\t      =====================""")
+
 
 def main():
     """
-    Retrieve a random answer from the words.txt file.
+    Retrieve an answer string from the words.txt file.
+    Create new instance of WordChecker, passing the answer as a parameter.
+    Create new instance of Game, passing the WordChecker instance as a parameter.
+    Start the game using the introduction() method.
     """
-    answer = get_word_from_file()
+    answer = get_answer_from_file()
+    game = Game(word_checker(answer))
     game.introduction()
+
 
 main()
